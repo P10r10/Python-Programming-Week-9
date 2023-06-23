7f0292e56e46c36660298874a7c89c21da7301b4
P10r10 <asantiago@keemail.me>
2023-06-23 12:09:20 +0100

    baby centre

---
+++ baby_centre.py
@@ -0,0 +1,15 @@
+class Person:
+    def __init__(self, name: str, age: int, height: int, weight: int):
+        self.name = name
+        self.age = age
+        self.height = height
+        self.weight = weight
+
+
+class BabyCentre:
+    def __init__(self):
+        self.number_of_weigh_ins = 0
+
+    def weigh(self, person: Person):
+        # return the weight of the person passed as an argument
+        return -1
